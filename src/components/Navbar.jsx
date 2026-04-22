9a76f743f41e7a4e37ae467a95ac9842eb6144aa
Diogo Piteira Castelos <main@diogopcastelos.pt>
2026-04-22 12:27:01 +0100

    upd

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import logo from "../assets/image.png";
+import logo from "../assets/image.svg";
 
 const Navbar = () => {
   const navigate = useNavigate();
 
   return (
     <nav className="flex items-center justify-between px-6 py-4 bg-peachz text-white shadow-md">
-      //TODO: Logo, Home e Update
+      //TODO: ADD LOGO, HOME AND UPDATE BUTTONS
     </nav>
   );
 };
